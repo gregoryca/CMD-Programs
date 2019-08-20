912ed1e208c05663399dfb326d8f0a8be118ea5f
gregory koeiman <aquiles.craane@gmail.com>
2019-08-20 09:24:07 +0200

    Made changes to the options, and i made it able to check out all the commands

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,6 +8,8 @@ namespace HelloWorld
 {
     class Program
     {
+        private static string answer;
+        private static string website;
 
         static void Main(string[] args)
         {
@@ -15,44 +17,99 @@ namespace HelloWorld
             string con = "/K ipconfig";
             string ren = "/K ipconfig /renew";
             string rel = "/K ipconfig /release";
-            string LanDisable = "/K netsh interface set interface" + " " + "LAN0" + " " + "admin=disable";
+            string flush = "/K ipconfig /flushdns";
+            string register = "/K ipconfig /registerdns";
+            string tracert = "/K  tracert -4" + website + "";
+            string LanDisable = "/netsh interface set interface" + " " + "LAN0" + " " + "admin=disable";
 
+            //workflow choices
 
-            for(int i = 0; i < 64; i++)
+            int inputAnswer = Convert.ToInt32(answer);
+
+            //CMD choice options
+            for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine("Choose your ipconfig script: 1 Ipconfig, 2 Release, 3 Renew, 4 Disable Lan, ");
-                string choice = Console.ReadLine();
-                if(choice == "")
+                Console.WriteLine("Check all CMD options build in");
+                string answer = Console.ReadLine();
+                if(answer == "")
                 {
-                    Console.WriteLine("No input detected");
-                    i = 0;
-                    
+                    for (int a = 0; a < 64; a++)
+                    {
+                        Console.WriteLine("Choose your ipconfig script: 1 Ipconfig, 2 Release, 3 Renew, 4 Register DNS, 5 Flush DNS, 6 Tracert");
+                        string input = Console.ReadLine();
+                        Console.WriteLine(input);
+                        if (input == "")
+                        {
+                            Console.WriteLine("No input detected");
+                            a = -1;
+
+                        }
+                        else
+                        {
+                            int caseSwitch = Convert.ToInt32(input);
+
+                            switch (caseSwitch)
+                            {
+                                default:
+                                    Process.Start("CMD.exe", con);
+
+                                    break;
+                                case 2:
+                                    Process.Start("CMD.exe", ren);
+
+                                    break;
+                                case 3:
+                                    Process.Start("CMD.exe", rel);
+
+                                    break;
+                                case 4:
+                                    Process.Start("CMD.exe", register);
+
+                                    break;
+                                case 5:
+                                    Process.Start("CMD.exe", flush);
+                                    break;
+                                case 6:
+                                    Console.WriteLine("type the website http address to start tracert");
+                                    string website = Console.ReadLine();
+                                    Process.Start("CMD.exe", tracert);
+                                    break;
+                            }
+                        }
+                    }
                 }
-
-                Console.WriteLine(i);
-                int caseSwitch = Convert.ToInt32(choice);
-
-                switch (caseSwitch)
+                else
                 {
-                    default:
-                        Process.Start("CMD.exe", con);
-                        i = 0;
-                        break;
-                    case 2:
-                        Process.Start("CMD.exe", ren);
-                        i = 0;
-                        break;
-                    case 3:
-                        Process.Start("CMD.exe", rel);
-                        i = 0;
-                        break;
-                    case 4:
-                        Process.Start("CMD.exe", LanDisable);
-                        i = 0;
-                        break;
+                    switch (inputAnswer)
+                    {
+                        default:
+                            Console.WriteLine(con);
+
+                            break;
+                        case 2:
+                            Console.WriteLine(ren);
+
+                            break;
+                        case 3:
+                            Console.WriteLine(rel);
+
+                            break;
+                        case 4:
+                            Console.WriteLine(flush);
+
+                            break;
+                        case 5:
+                            Console.WriteLine(register);
+                            break;
+                        case 6:
+                            Console.WriteLine(LanDisable);
+                            break;
+                    }
                 }
-                System.Diagnostics.Process.Start("CMD.exe", choice);
             }
-        }
+            Console.WriteLine("Choose you workflow");
+
+            System.Diagnostics.Process.Start("CMD.exe", answer);
+        }    
     }
 }
